3b7ed3cac535c828eb7bf932db04ba94d49356ae
Andriy Zhabura <andriy@zhabura.com>
2016-10-03 19:57:22 +0300

    add credentials and CMakeLists.txt

--- VulkanTutorial_01/VulkanTutorial/src/ZVK_Application.cpp
+++ VulkanTutorial_01/VulkanTutorial/src/ZVK_Application.cpp
@@ -1,3 +1,32 @@
+/* ZVK_Application.cpp
+ * VulkanTutorial project
+ *
+ * Created by Andriy Zhabura on 02-Oct-2016.
+ * Last modified on 03-Oct-2016.
+ */
+
+/*
+ * Copyright © 2016 Andriy Zhabura
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting documentation, and
+ * that the name of the copyright holders not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  The copyright holders make no representations
+ * about the suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
+ */
+ 
 #include "ZVK_Application.h"
 #include <vector>
 
@@ -10,7 +39,7 @@ ZVK_Application::ZVK_Application()
 #endif
 
 	app_info = vk::ApplicationInfo()
-		.setPApplicationName("Vulkan C++ Program Template")
+		.setPApplicationName("Vulkan C++ Tutorial")
 		.setApplicationVersion(1)
 		.setPEngineName("LunarG SDK")
 		.setEngineVersion(1)
--- VulkanTutorial_01/VulkanTutorial/src/main.cpp
+++ VulkanTutorial_01/VulkanTutorial/src/main.cpp
@@ -1,12 +1,48 @@
+/* main.cpp
+ * VulkanTutorial project
+ *
+ * Created by Andriy Zhabura on 02-Oct-2016.
+ * Last modified on 03-Oct-2016.
+ */
+
+/*
+ * Copyright © 2016 Andriy Zhabura
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting documentation, and
+ * that the name of the copyright holders not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  The copyright holders make no representations
+ * about the suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
+ */
+ 
 #include <iostream>
 
 #include "ZVK_Application.h"
 
 int main(int argc, char **argv)
+try
 {
 	ZVK_Application app;
 
-	std::cout << "hi\n";
+	std::cout << "Vulkan Application was successfully created\n";
 
 	return 0;
+}
+catch (...)
+{
+	std::cout << "Problem with Vulkan Application creation\n";
+
+	return 1;
 }
