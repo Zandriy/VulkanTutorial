a3e7fec0228d5282cbb30215ec8909f8f369dcb6
Andriy Zhabura <andriy@zhabura.com>
2016-10-04 20:50:41 +0300

    Finish VulkanTutorial2

---
+++ VulkanTutorial_02/VulkanTutorial/src/main.cpp
@@ -0,0 +1,80 @@
+/* main.cpp
+ * VulkanTutorial project
+ *
+ * Created by Andriy Zhabura on 02-Oct-2016.
+ * Last modified on 04-Oct-2016.
+ */
+
+/*
+ * Copyright © 2016 Andriy Zhabura
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting documentation, and
+ * that the name of the copyright holders not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  The copyright holders make no representations
+ * about the suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
+ */
+ 
+#include <iostream>
+
+#include "ZVK_Application.h"
+
+int main(int argc, char **argv)
+try
+{
+	ZVK_Application app;
+
+	std::cout << "Vulkan Application was successfully created.\n";
+	std::vector<std::string> names = app.get_LayerPropertiesNames();
+	if (names.size())
+	{
+		std::cout << "Available layers:\n";
+		for (auto name : names)
+		{
+			std::cout << "    " << name << "\n";
+		}
+	}
+	names = app.get_ExtensionPropertiesNames();
+	if (names.size())
+	{
+		std::cout << "Available layers extensions:\n";
+		for (auto name : names)
+		{
+			std::cout << "    " << name << "\n";
+		}
+	}
+	std::cout << "Found " << app.get_PhysicalDevicesQty()
+		<< " physical device" << (app.get_PhysicalDevicesQty() > 1 ? "s.\n" : ".\n");
+
+	return 0;
+}
+catch (std::bad_alloc&)
+{
+	std::cerr << "Problem with Vulkan Application creation\n";
+
+	return 1;
+}
+catch (std::domain_error&)
+{
+	std::cerr << "Problem with properties in the Vulkan domain\n";
+
+	return 2;
+}
+catch (...)
+{
+	std::cout << "Uncaught exception discovered\n";
+
+	return -1;
+}
